a5a6a4792ea90426a90d3cbb2665cb67b56ef01b
Andre Pato <anpato1994@gmail.com>
2023-04-03 22:59:56 -0400

    init

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+
 const fs = require('fs');
 const path = require('path');
 try {
+  const octokit = github.getOctokit();
+
   const targetDir = core.getInput('target_dir');
-  const files = fs.readdirSync(path.join(__dirname, targetDir));
+  const files = fs.readdirSync(path.join(core.toPlatformPath(), targetDir));
   core.setOutput('flags', files);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  const payload = JSON.stringify(github.wo, undefined, 2);
   console.log(payload);
 } catch (error) {
   core.setFailed(error.message);
