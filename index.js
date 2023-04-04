c31edcb92baa87a5a78995bec80cbec45049cad1
Andre Pato <anpato1994@gmail.com>
2023-04-03 22:51:10 -0400

    init

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const path = require('path');
 try {
   const targetDir = core.getInput('target_dir');
-  const files = fs.readdirSync(targetDir);
+  const files = fs.readdirSync(path.join(__dirname, targetDir));
   core.setOutput('flags', files);
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(payload);
