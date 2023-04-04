420ca3fb1c3e977d0dcfbd96154ff4cd44dd77cf
Andre Pato <anpato1994@gmail.com>
2023-04-03 22:38:15 -0400

    add modules

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ try {
   const targetDir = core.getInput('target_dir');
   console.log(targetDir);
   core.setOutput('flags', []);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(payload);
 } catch (error) {
   core.setFailed(error.message);
 }
